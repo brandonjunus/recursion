6b89bb72d4c91c2b008ee2f238880c189dc63f98
Brandon Junus <brandonjunus2@gmail.com>
2018-07-18 18:01:25 -0700

    Commit to complete bare minimum requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+  var elementsWithClass = [];
+
+  function findElementsWithClass(element) {
+    if (element.classList && element.classList.contains(className)) {
+      elementsWithClass.push(element);
+    }
+    if (element.childNodes) {
+      _.each(element.childNodes, function(item) {
+        findElementsWithClass(item);
+      });
+    }
+  }
+  findElementsWithClass(document);
+  return elementsWithClass;
+}
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,34 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
+
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+// var stringifyJSON = function(obj) {
+//   for (key in obj){
+    
+//   }
+// };
+var stringifyJSON = function(obj){
+  if (typeof obj === "string"){
+    return '"' + obj + '"';
+  } else if(obj === null){
+    return "null";
+  }
+  if (Array.isArray(obj)){
+    var myArray = []
+    for (let i = 0; i < obj.length; i++){
+      myArray.push(stringifyJSON(obj[i]));
+    }
+    return "[" + myArray.join() + "]";
+  } else if (typeof obj === "object"){
+    var myObj = []
+    for(let key in obj){
+      if(typeof obj[key] !== "function" && obj[key] !== undefined){
+      myObj.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]))
+      }
+    }
+    return "{" + myObj.join() + "}";
+  }
+  return obj + '';
+}
